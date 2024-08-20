23ff45c59903a99cf423113bd50b231d51073b5e
Aneesh Anil <aneeshanil008@gmail.com>
2024-08-20 18:42:12 +0530

    write logic to extract, clean and save into a csv file.

--- indian_kindgoms_wiki.py
+++ indian_kindgoms_wiki.py
@@ -1,6 +1,7 @@
 import json
 import os
 from os import path
+import sys
 from urllib.request import urlopen
 
 def scrape(url, title) -> str:
@@ -30,7 +31,6 @@ def scrape(url, title) -> str:
         f.write(html)
     
     return html_saved_path
-
 
 if __name__ == '__main__':
     url = 'https://en.wikipedia.org/wiki/List_of_Hindu_empires_and_dynasties'
@@ -68,9 +68,94 @@ if __name__ == '__main__':
 
         # print('----')
 
+    csv_headers = csv_headers[:-1]
+
     print('csv headers: ', csv_headers)
         # print(header.get_text().strip())
     
-    for kingdom in kingdoms:
-        pass
+    csv_rows = []
+    for i, kingdom in enumerate(kingdoms):
+        # if i != 3:
+        #     continue
+        krow = []
+        if len(kingdom) != 14:
+            raise Exception(f'expected <tr> with len 14. Got with len: {len(kingdom)}')
+        # print(kingdom)
+        # print(dir(kingdom))
+        # print(next(kingdom))
+        extracted_kingdom_row_texts = []
+        for i, kdata in enumerate(kingdom):
+            # print(f'idx: {i}. data: {kdata.get_text()}. type: {kdata.__class__}')
+
+            # print(len(kdata))
+            value: str = kdata.get_text()
+            value = value.strip()
+            extracted_kingdom_row_texts.append(value)
+            # if len(value) > 0:
+            #     krow.append(value)
+            
+            # print(value)
+        if len(extracted_kingdom_row_texts) != 14:
+            raise Exception(f'expected 14 values to be extracted. Got: {len(extracted_kingdom_row_texts)}')
+        # print(extracted_kingdom_row_texts)
+        for idx, t in enumerate(extracted_kingdom_row_texts):
+            if idx in [1, 3, 5, 7, 9, 11]:
+                krow.append(t)
+        csv_rows.append(krow)
+        # print(kingdom)
+        # if i == 5:
+        #     break
+        # break
+    # sys.exit(1)
+
+    print(csv_rows)
+
+    # sys.exit(1)
+
+    print(f'Parsed {len(csv_rows)} kingdoms.')
+
+    non_conforming_indices = []
+
+    for idx, row in enumerate(csv_rows):
+        if len(row) != 6:
+            print('-'*20)
+            print(f'Index: {idx}. Len: {len(row)}')
+            print(row)
+            print('-'*20)
+            non_conforming_indices.append(idx)
+            continue
+        if idx == 56:
+            print('+'*20)
+            print(row)
+            print('+'*20)
+        part_of: str = row[5]
+        part_of = part_of.replace('\n', '\xa0')
+        part_of = part_of.split('\xa0')
+        part_of = list(filter(lambda n : n != '', part_of))
+        part_of = ','.join(part_of)
+        row[5] = part_of
+
+        languages: str = row[4]
+        languages = languages.replace('\n', '\xa0')
+        languages = languages.split('\xa0')
+        languages = list(filter(lambda n : n != '', languages))
+        languages = ','.join(languages)
+        row[4] = languages
+
+        # print(part_of)
+
+    # csv_data = [csv_headers]
+    print(f'Non conforming Indices: {non_conforming_indices}')
+
+    out_csv_path = path.join('out', 'kingdoms.csv')
+
+    with open(out_csv_path, 'wt') as f:
+        import csv
+        writer = csv.writer(f)
+        writer.writerow(csv_headers)
+        writer.writerows(csv_rows)
+    
+    print(f'data written to {out_csv_path}')
+
+
 
