642858b9167fc7e1d353284fad3735395d38dc1b
Aneesh Anil <aneeshanil008@gmail.com>
2024-08-20 17:22:25 +0530

    parsed out table headers from html table

--- indian_kindgoms_wiki.py
+++ indian_kindgoms_wiki.py
@@ -1,3 +1,4 @@
+import json
 import os
 from os import path
 from urllib.request import urlopen
@@ -34,4 +35,42 @@ def scrape(url, title) -> str:
 if __name__ == '__main__':
     url = 'https://en.wikipedia.org/wiki/List_of_Hindu_empires_and_dynasties'
     title = 'indian_kingdoms_wiki'
-    scrape(url, title)
+    # scrape(url, title)
+
+    from bs4 import BeautifulSoup
+
+    with open(path.join("out", f"{title}.html")) as f:
+        html = f.read()
+        soup = BeautifulSoup(html, "html.parser")
+
+    kingdoms_table = soup.find("table", class_="wikitable")
+
+    body = list(kingdoms_table)[-1]
+    # with open('out/kinkdoms_table.html', 'w') as f:
+    #     f.write(str(body))
+
+    rows = body.find_all('tr')
+
+    headers = rows[0]
+    kingdoms = rows[1:]
+
+    csv_headers = []
+
+    for header in headers:
+        # print(header)
+        value: str = header.text
+        value = value.strip()
+        # print(f"Value: {value}")
+        # print(dir(header))
+        # print(header.get_text())
+        if len(value) > 0:
+            csv_headers.append(value)
+
+        # print('----')
+
+    print('csv headers: ', csv_headers)
+        # print(header.get_text().strip())
+    
+    for kingdom in kingdoms:
+        pass
+
